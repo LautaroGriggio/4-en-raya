9afdf04d335d3f6c7c14fdff141c216f73d8d969
gonzalodfuentes <130400825+gonzalodfuentes@users.noreply.github.com>
2024-07-01 13:23:08 -0300

    Update modoContrarreloj.h

--- modoContrarreloj.h
+++ modoContrarreloj.h
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <windows.h>
-#include <cstdlib>  // Para la funciÛn exit()
+#include <cstdlib>  // Para la funci√≥n exit()
 #include <chrono>   // Para el temporizador
 #include <thread>   // Para el temporizador
 #include <atomic>   // Para el temporizador
@@ -28,8 +28,10 @@
 using namespace std;
 
 int TIEMPO_TOTAL_JUGADOR = 40; // Tiempo total en segundos para cada jugador
- 
-int tablero[6][7] = {0};
+
+const int FILAS2 = 6;
+const int COLUMNAS2 = 7;
+int tablero2[6][7] = {0};
 
 // Variables para el tiempo restante de cada jugador
 atomic<int> tiempoRestanteJugador1(TIEMPO_TOTAL_JUGADOR);
@@ -39,15 +41,9 @@ atomic<int> tiempoRestanteJugador2(TIEMPO_TOTAL_JUGADOR);
 atomic<bool> turnoActivoJugador1(false);
 atomic<bool> turnoActivoJugador2(false);
 
-int FILAS;
-int COLUMNAS;
 
-
-void line() 
+void line2() 
 {
-	FILAS = 6;
-	COLUMNAS = 7;
-	
     for (int k = 0; k < 7; ++k) 
 	{
         printf(AZUL_F AZUL_T "+---" RESET_COLOR);
@@ -56,11 +52,11 @@ void line()
     printf("\n");
 }
 
-void dibujarTablero() 
+void dibujarTablero2() 
 {
-    system("cls");  // Limpiar la consola antes de dibujar el tablero
+    system("cls");  // Limpiar la consola antes de dibujar el tablero2
 
-    line();
+    line2();
 
     for (int i = 0; i < 6; i++) 
 	{
@@ -68,45 +64,45 @@ void dibujarTablero()
 		{
             printf(AZUL_F AZUL_T "|" RESET_COLOR);
 
-            if (tablero[i][j] == 0) 
+            if (tablero2[i][j] == 0) 
 			{
-                printf(BLANCO_F BLANCO_T " %d ", tablero[i][j]);
+                printf(BLANCO_F BLANCO_T " %d ", tablero2[i][j]);
             } 
-			else if (tablero[i][j] == 1) 
+			else if (tablero2[i][j] == 1) 
 			{
-                printf(AMARILLO_F AMARILLO_T " %d ", tablero[i][j]);
+                printf(AMARILLO_F AMARILLO_T " %d ", tablero2[i][j]);
             } 
-			else if (tablero[i][j] == 2) 
+			else if (tablero2[i][j] == 2) 
 			{
-                printf(ROJO_F ROJO_T " %d ", tablero[i][j]);
+                printf(ROJO_F ROJO_T " %d ", tablero2[i][j]);
             }
         }
         printf(AZUL_F AZUL_T "|" RESET_COLOR);
         printf("\n");
 
-        // Imprimir lÌneas separadoras entre filas
+        // Imprimir l√≠neas separadoras entre filas
         if (i < 5) 
 		{
-            line();
+            line2();
         }
     }
 
-    line();
+    line2();
 }
 
-bool verificarGanador(int fila, int col) 
+bool verificarGanador2(int fila, int col) 
 {
-    int jugadorActual = tablero[fila][col];
+    int jugadorActual = tablero2[fila][col];
     int contador = 1;  // Contador para contar fichas consecutivas del mismo jugador
 
     // Verificar horizontalmente hacia la derecha
-    for (int j = col + 1; j < COLUMNAS && tablero[fila][j] == jugadorActual; ++j) 
+    for (int j = col + 1; j < COLUMNAS2 && tablero2[fila][j] == jugadorActual; ++j) 
 	{
         ++contador;
     }
 
     // Verificar horizontalmente hacia la izquierda
-    for (int j = col - 1; j >= 0 && tablero[fila][j] == jugadorActual; --j) 
+    for (int j = col - 1; j >= 0 && tablero2[fila][j] == jugadorActual; --j) 
 	{
         ++contador;
     }
@@ -116,13 +112,13 @@ bool verificarGanador(int fila, int col)
     contador = 1;
 
     // Verificar verticalmente hacia arriba
-    for (int i = fila - 1; i >= 0 && tablero[i][col] == jugadorActual; --i) 
+    for (int i = fila - 1; i >= 0 && tablero2[i][col] == jugadorActual; --i) 
 	{
         ++contador;
     }
 
     // Verificar verticalmente hacia abajo
-    for (int i = fila + 1; i < FILAS && tablero[i][col] == jugadorActual; ++i) 
+    for (int i = fila + 1; i < FILAS2 && tablero2[i][col] == jugadorActual; ++i) 
 	{
         ++contador;
     }
@@ -132,12 +128,12 @@ bool verificarGanador(int fila, int col)
     contador = 1;
 
     // Verificar diagonal hacia abajo (izquierda a derecha)
-    for (int i = fila + 1, j = col + 1; i < FILAS && j < COLUMNAS && tablero[i][j] == jugadorActual; ++i, ++j) 
+    for (int i = fila + 1, j = col + 1; i < FILAS2 && j < COLUMNAS2 && tablero2[i][j] == jugadorActual; ++i, ++j) 
 	{
         ++contador;
     }
 
-    for (int i = fila - 1, j = col - 1; i >= 0 && j >= 0 && tablero[i][j] == jugadorActual; --i, --j) 
+    for (int i = fila - 1, j = col - 1; i >= 0 && j >= 0 && tablero2[i][j] == jugadorActual; --i, --j) 
 	{
         ++contador;
     }
@@ -147,19 +143,19 @@ bool verificarGanador(int fila, int col)
     contador = 1;
 
     // Verificar diagonal hacia arriba (izquierda a derecha)
-    for (int i = fila - 1, j = col + 1; i >= 0 && j < COLUMNAS && tablero[i][j] == jugadorActual; --i, ++j) 
+    for (int i = fila - 1, j = col + 1; i >= 0 && j < COLUMNAS2 && tablero2[i][j] == jugadorActual; --i, ++j) 
 	{
         ++contador;
     }
 
-    for (int i = fila + 1, j = col - 1; i < FILAS && j >= 0 && tablero[i][j] == jugadorActual; ++i, --j) 
+    for (int i = fila + 1, j = col - 1; i < FILAS2 && j >= 0 && tablero2[i][j] == jugadorActual; ++i, --j) 
 	{
         ++contador;
     }
 
     if (contador >= 4) return true;  // Ganador encontrado diagonal hacia arriba
 
-    return false;  // No hay ganador en ninguna direcciÛn
+    return false;  // No hay ganador en ninguna direcci√≥n
 }
 
 void mostrarTiempoRestante(int jugador, atomic<int>& tiempoRestante, atomic<bool>& turnoActivo) 
@@ -193,7 +189,7 @@ int contraReloj()
 
     while (!juegoTerminado) 
 	{
-        dibujarTablero();
+        dibujarTablero2();
 
         int colum;
         int columna;
@@ -210,26 +206,26 @@ int contraReloj()
 
         columna = colum - 1;
 
-        if (columna < 0 || columna >= COLUMNAS) 
+        if (columna < 0 || columna >= COLUMNAS2) 
 		{
             system("cls");
-            cout << "Columna inv·lida. Por favor, ingresa un n˙mero entre 1 y 7." << endl;
+            cout << "Columna inv√°lida. Por favor, ingresa un n√∫mero entre 1 y 7." << endl;
             turnoActivo = false;
             continue;
         }
 
         system("cls");
 
-        for (int i = FILAS - 1; i >= 0; --i) 
+        for (int i = FILAS2 - 1; i >= 0; --i) 
 		{
-            if (tablero[i][columna] == 0) 
+            if (tablero2[i][columna] == 0) 
 			{
-                tablero[i][columna] = jugador;
-                if (verificarGanador(i, columna)) 
+                tablero2[i][columna] = jugador;
+                if (verificarGanador2(i, columna)) 
 				{
-                    dibujarTablero();
+                    dibujarTablero2();
                     cout << endl;
-                    cout << "°Jugador " << jugador << " ha ganado!" << endl;
+                    cout << "¬°Jugador " << jugador << " ha ganado!" << endl;
                     juegoTerminado = true;
                 }
                 break;
@@ -243,9 +239,8 @@ int contraReloj()
     tMostrarTiempo1.join();
     tMostrarTiempo2.join();
 
-//    // Limpiar el tablero y volver al men˙ principal
-//    tablero.assign(6, vector<int>(7, 0));
+//    // Limpiar el tablero2 y volver al men√∫ principal
+//    tablero2.assign(6, vector<int>(7, 0));
 
     return 0;
 }
-
